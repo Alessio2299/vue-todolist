16bb2aaefd6b9faabfaccf09f34e51150179c199
Alessio Napoli <alessionapoli99@icloud.com>
2022-02-11 16:07:13 +0100

    Added methods and into HTML added @click, v-for

--- js/main.js
+++ js/main.js
@@ -7,5 +7,23 @@ var app = new Vue({
       done: "false"
     },
     toDoList: [],
+  },
+  methods: {
+    addToDo(){
+      this.toDoList.push(this.toDo);
+      this.toDo = {whatToDo: "", done: "false"};
+    },
+    removeToDo(indice){ 
+      this.toDoList.splice(indice,1);
+    },
+    doneOrNot(){
+      if(this.toDo.done == "false"){
+        this.toDo.done = "true";
+        console.log(this.toDo.done);
+      } else{
+        this.toDo.done = "false";
+        console.log(this.toDo.done);
+      }
+    }
   }
 })
